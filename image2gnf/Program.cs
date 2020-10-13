013739a9557e59678af28472be994147e63c36ed
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2020-10-13 14:38:10 +0200

    Init code

---
+++ image2gnf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace image2gnf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
